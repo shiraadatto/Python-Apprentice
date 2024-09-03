cc17f7e1d7287ca9e0125b688e95010eba65eb9d
shiraadatto <149182666+shiraadatto@users.noreply.github.com>
2024-09-03 21:43:31 +0000

    Modified

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -7,4 +7,23 @@ from the section "Change the Turtle Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup (width=600, height=600)    # Set the size of the window
 
+tina = turtle.Turtle()
+
+
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+set_turtle_image(tina, "leaguebot_bolt.gif")
+
+turtle.exitonclick()
