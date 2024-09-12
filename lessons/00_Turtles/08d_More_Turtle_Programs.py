4791b3e48ac9a08e4124673f182a44a63ee8f372
shiraadatto <149182666+shiraadatto@users.noreply.github.com>
2024-09-12 21:44:44 +0000

    09d

--- lessons/00_Turtles/08d_More_Turtle_Programs.py
+++ lessons/00_Turtles/08d_More_Turtle_Programs.py
@@ -13,3 +13,27 @@ Use this code to get a random x and y location
     y = random.randint(-300, 300)
 
 """
+import turtle as turtle
+import random
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('white')
+
+t = turtle.Turtle()
+t.penup()
+t.shape("turtle")
+
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+
+def turtle_clicked(t, x, y):
+    print('turtle clicked!')
+    
+    x = random.randint(-300, 300)
+    y = random.randint(-300, 300)
+    print(x,y)
+    t.goto(x,y)
+
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
