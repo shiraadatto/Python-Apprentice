51cd4043345a7ac57a0d1b743fa65dedc0529bc1
shiraadatto <149182666+shiraadatto@users.noreply.github.com>
2024-10-15 21:48:23 +0000

    worked on sky line

--- lessons/00_Turtles/03a_Turtle_Tricks.py
+++ lessons/00_Turtles/03a_Turtle_Tricks.py
@@ -14,7 +14,7 @@ import random                         # Tell Python we want to work with the tur
 turtle.setup (width=600, height=600)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
-
+tina.speed(10)
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
@@ -32,21 +32,26 @@ def drawStar():
     tina.end_fill()
     tina.penup()
 
-for i in range(10):
+for i in range(30):
     drawStar()
 
-tina.goto(-300,-200)
+tina.goto(-300,153)
+
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
 
 def drawBuilding():
-    tina.pencolor("red")
+    tina.pencolor(colors[random.randint(0,5)])
+    tina.fillcolor(colors[random.randint(0,5)])
     tina.pendown()
+    tina.begin_fill()
     tina.forward(100)
     tina.right(90)
-    tina.forward(50)
+    tina.forward(300)
+    tina.right(90)
+    tina.forward(300)
     tina.right(90)
     tina.forward(100)
-    tina.right(90)
-    tina.forward(50)
+    tina.end_fill()
     tina.penup()
 
 for i in range(1):
