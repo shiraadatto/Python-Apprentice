fc236b443e858e13d75e5dcfbd35a2ba80f1ea60
shiraadatto <149182666+shiraadatto@users.noreply.github.com>
2024-11-14 22:46:23 +0000

    ifs and elifs

--- lessons/01_Types_and_Logic/05_Infuriating_Calculator.py
+++ lessons/01_Types_and_Logic/05_Infuriating_Calculator.py
@@ -22,19 +22,34 @@ you can ask for a string with simpledialog.askstring().
 """
 
 # Import the required modules
+from tkinter import messagebox, simpledialog, Tk # import required modules
 
 # Create a window object
+window = Tk()     # Create a window object
+ 
 
 # Hide the window, hint: use the withdraw method
+window.withdraw()
 
 # Ask the user for the first number   
+number1 = simpledialog.askinteger('Pick a Number', "Pick a Number")
 
 # Ask the user for the second number
+number2 = simpledialog.askinteger('Pick a Number', "Pick another Number")
 
 # Ask the user for the math operation
-
+mathOperation = simpledialog.askstring('Pick a Math Operation', "Pick a Math Operation")
 # Use if-elif-else statements to provide the desired math operation on the numbers and display the result.
-
+if mathOperation == "addition":
+   messagebox.showinfo('Your Operation', str(number1) + " + " + str(number2) + " = " + str(number1 + number2))
+elif mathOperation == "subtraction":
+   messagebox.showinfo('Your Operation', str(number1) - " - " + str(number2) + " = " + str(number1 - number2))
+elif mathOperation == "multiplication":
+   messagebox.showinfo('Your Operation', str(number1) + " * " + str(number2) + " = " + str(number1 * number2))
+elif mathOperation == "division": 
+   messagebox.showinfo('Your Operation', str(number1) + " / " + str(number2) + " = " + str(number1 / number2))
+else:
+   messagebox.showerror()
 # If the user enters an unknown operation, display an error message. ( use messagebox.showerror()
 
 # Keep the window open
